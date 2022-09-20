fb2c3c71d136207523cb36a8fcaaabc0532ccc72
Connor McCue <connor.mccue@comcast.net>
2022-09-20 14:43:15 -0700

    complete

--- reduce.js
+++ reduce.js
@@ -2,9 +2,15 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brands) => { acc.push(...brands.shoes);
+    return acc;
+  }, []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  return dogs.reduce((acc, dogs) => acc += dogs.age, 0) / dogs.length;
+};
