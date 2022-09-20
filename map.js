5e6b96e999ca976cc682a53359c09858ddb749e9
Connor McCue <connor.mccue@comcast.net>
2022-09-20 14:00:53 -0700

    map done

--- map.js
+++ map.js
@@ -2,16 +2,22 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  return countries.map(({ id, name, iso2, iso3, local_name, continent, ...rest }) => name);
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  return countries.map(({ id, name, iso2, iso3, local_name, continent, ...rest }) => ([name, iso2]));
+};
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
 // REQS: use .map
 
-export const getBrands = (brands) => {};
+export const getBrands = (brands) => {
+  return brands.map(({ brand, shoes, slogan, logo, headquarters, ...rest }) => (brand));
+};
